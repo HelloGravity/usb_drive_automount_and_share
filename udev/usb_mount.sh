537f0338b89a54a8db051c02c912e3e5af6adac9
Yaron <yaron.mic@gmail.com>
2015-08-08 14:34:59 +0300

    Update usb_mount.sh

--- udev/usb_mount.sh
+++ udev/usb_mount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# USAGE: usb_mount.sh [dir name (LaCie...)] [device name (sdb...)] [mount_uptions]
+# USAGE: usb_mount.sh [dir name (device label...)] [device name (sdb...)] [mount_uptions]
 
 /etc/udev/usb_mounter.sh $1 $2 $3 &
